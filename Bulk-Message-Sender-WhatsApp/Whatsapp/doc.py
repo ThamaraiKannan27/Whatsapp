cc47a1cad2946e20d9cbc6a8336a3da848ff72f0
ThamaraiKannan27 <127646447+ThamaraiKannan27@users.noreply.github.com>
2023-09-08 11:13:03 +0530

    Update doc.py

--- Bulk-Message-Sender-WhatsApp/Whatsapp/doc.py
+++ Bulk-Message-Sender-WhatsApp/Whatsapp/doc.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
@@ -7,20 +6,13 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.chrome.options import Options
 
+options = webdriver.ChromeOptions()
+options.add_argument("--user-data-dir=/home/username/.config/google-chrome")
 
-options = Options()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-options.add_argument("--profile-directory=Default")
-options.add_argument("--user-data-dir=/var/tmp/chrome_user_data")
+driver = webdriver.Chrome(options=options)  
 
+image_path="" #document path
 
-login_time = 10               
-new_msg_time = 5            
-send_msg_time = 3         
-action_time = 2               
-image_path = "C:\Ragul_P R_Resume_10-04-2023-21-06-19 (1).pdf"    #document path
-
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 driver.get('https://web.whatsapp.com')
 input("Press ENTER...")
 
@@ -29,20 +21,23 @@ with open('message.txt', 'r') as file:
 
 link = 'https://web.whatsapp.com'
 driver.get(link)
+
 with open('numbers.txt', 'r') as file:
     for n in file.readlines():
         num = n.rstrip()
         link = f'https://web.whatsapp.com/send/?phone={num}'
         driver.get(link)
-        time.sleep(new_msg_time)
-        attach_btn = driver.find_element(By.CLASS_NAME, '_1OT67')
-        attach_btn.click()
-        time.sleep(action_time)
-        msg_input = driver.find_element(By.CLASS_NAME, '_1CGek input')
-        msg_input.send_keys(image_path)
-        time.sleep(action_time)
+        time.sleep(8)
         actions = ActionChains(driver)
+        attach_btn= driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div')
+        attach_btn.click()
+        image_btn= driver.find_element(By.XPATH,'/html/body/div[1]/div/div/div[5]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/ul/div/div[1]/li/div/input')
+        image_btn.send_keys(image_path)
+        time.sleep(2)
+        for line in msg.split('\n'):
+            actions.send_keys(line)
+            actions.key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT)
         actions.send_keys(Keys.ENTER)
         actions.perform()
-        time.sleep(send_msg_time)
+        time.sleep(5)
 driver.quit()
